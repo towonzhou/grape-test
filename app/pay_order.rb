caaabcbbc86b8d26ec04056ca0f85c65d40bbe54
zhouhuan <towonzhou@gmail.com>
2014-08-23 11:45:41 +0800

    修改数据库的配置

--- app/pay_order.rb
+++ app/pay_order.rb
@@ -1,11 +1,10 @@
 # -*- encoding: UTF-8 -*-
 ##############################################################
-# File Name: config/routes.rb
+# File Name: app/pay_order.rb
 # Author: zhouhuan
 # mail: towonzhou@gmail.com
-# Created Time: 六  8/23 10:51:48 2014
+# Created Time: 六  8/23 11:42:34 2014
 ##############################################################
 
-Monterail::Application.routes.draw do
-  mount AllApi::Api => '/api'
+class PayOrder < ActiveRecord::Base
 end
